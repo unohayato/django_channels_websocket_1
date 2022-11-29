4d2f095781913335c46f9feeca5ea76300eb01ee
unohayato <mamidori1p1p@gmail.com>
2022-11-29 17:18:06 +0900

    set

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/unohayato/Documents/GitHub/django_channels_websocket_1/myvenv/bin/python3.10
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
