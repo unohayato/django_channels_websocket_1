0002c0082190de0d1f771aab94d0a63ef3b348a2
unohayato <mamidori1p1p@gmail.com>
2022-11-29 20:45:43 +0900

    can't resolve an error

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
     'channels',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chat',
 ]
+
+
+ASGI_APPLICATION = 'mywebsite.asgi.application'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
